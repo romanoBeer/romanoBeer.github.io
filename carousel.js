d7e51515a930b7a632e036a304132cd344381715
romanoBeer <99826861+romanoBeer@users.noreply.github.com>
2023-06-23 15:34:03 +0200

    added a form

--- carousel.js
+++ carousel.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:10,
-        nav:true,
+        nav:false,
         lazyLoad:true,
         autoplay:true,
         autoplayTimeout:2000,
